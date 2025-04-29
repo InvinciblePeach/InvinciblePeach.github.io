de25c947835ec5943c528c58f435333564ed695d
410 <invinciblepeach@gmail.com>
2025-04-29 20:49:27 +0800

    Site updated: 2025-04-29 20:49:27

--- js/img-lazyload.js
+++ js/img-lazyload.js
@@ -1,1 +1,10 @@
-(()=>{for(let e of document.querySelectorAll("img[lazyload]"))Fluid.utils.waitElementVisible(e,function(){e.removeAttribute("srcset"),e.removeAttribute("lazyload")},CONFIG.lazyload.offset_factor)})(window);
+/* global Fluid, CONFIG */
+
+(function(window, document) {
+  for (const each of document.querySelectorAll('img[lazyload]')) {
+    Fluid.utils.waitElementVisible(each, function() {
+      each.removeAttribute('srcset');
+      each.removeAttribute('lazyload');
+    }, CONFIG.lazyload.offset_factor);
+  }
+})(window, document);
